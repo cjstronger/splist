b4df0aa7ee0210f014f92e078aa91bd9f726d958
cjstronger <clintjstrong@gmail.com>
2024-10-11 01:54:13 -0700

    test 5

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require(".");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+  console.error("Stack Trace:", reason.stack);
+  // Optionally, you can exit the process if needed
+  // process.exit(1);
+});
